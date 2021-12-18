97c2caac509a2963749cc4e0a28965a8460a1172
jameswilddev <jameswilddev@outlook.com>
2021-12-18 12:47:38 +0000

    Update dependencies.

--- nodejs.ts
+++ nodejs.ts
@@ -1,39 +1,71 @@
 import type { Document } from "@skitscript/types-nodejs";
 import * as fs from "fs";
 import * as path from "path";
-import { parse } from "@skitscript/parser-nodejs";
+import { identifierIsValid, parse } from "@skitscript/parser-nodejs";
 
 describe(`parse`, () => {
-  const documentCasesPath = path.join(__dirname, `document-cases`);
+  describe(`documents`, () => {
+    const casesPath = path.join(__dirname, `document-cases`);
 
-  const caseNames = fs.readdirSync(documentCasesPath);
+    const caseNames = fs.readdirSync(casesPath);
 
-  for (const caseName of caseNames) {
-    describe(caseName, () => {
-      for (const newline of [`\n`, `\r`, `\r\n`]) {
-        describe(`with a newline of ${JSON.stringify(newline)}`, () => {
-          let document: Document;
+    for (const caseName of caseNames) {
+      describe(caseName, () => {
+        for (const newline of [`\n`, `\r`, `\r\n`]) {
+          describe(`with a newline of ${JSON.stringify(newline)}`, () => {
+            let document: Document;
 
-          beforeAll(async () => {
-            const source = await fs.promises.readFile(
-              path.join(documentCasesPath, caseName, `input.skitscript`),
-              `utf8`
-            );
+            beforeAll(async () => {
+              const source = await fs.promises.readFile(
+                path.join(casesPath, caseName, `input.skitscript`),
+                `utf8`
+              );
 
-            document = parse(source.replace(/\n/g, newline));
+              document = parse(source.replace(/\n/g, newline));
+            });
+
+            it(`parses to the expected document`, async () => {
+              const outputText = await fs.promises.readFile(
+                path.join(casesPath, caseName, `output.json`),
+                `utf8`
+              );
+              const output = JSON.parse(outputText);
+
+              expect(document).toEqual(output);
+            });
           });
+        }
+      });
+    }
+  });
 
-          it(`parses to the expected document`, async () => {
-            const outputText = await fs.promises.readFile(
-              path.join(documentCasesPath, caseName, `output.json`),
-              `utf8`
-            );
-            const output = JSON.parse(outputText);
+  describe(`identifiers`, () => {
+    describe(`valid`, () => {
+      const cases = fs.readFileSync(
+        path.join(__dirname, `identifier-cases`, `valid.txt`),
+        `utf8`
+      );
 
-            expect(document).toEqual(output);
+      for (const line of cases.split(`\n`)) {
+        if (line.trim() !== ``) {
+          it(JSON.stringify(line), () => {
+            expect(identifierIsValid(line)).toBeTrue();
           });
+        }
+      }
+    });
+
+    describe(`invalid`, () => {
+      const cases = fs.readFileSync(
+        path.join(__dirname, `identifier-cases`, `invalid.txt`),
+        `utf8`
+      );
+
+      for (const line of cases.split(`\n`)) {
+        it(JSON.stringify(line), () => {
+          expect(identifierIsValid(line)).toBeFalse();
         });
       }
     });
-  }
+  });
 });
